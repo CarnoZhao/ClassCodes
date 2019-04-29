c92ef8a362ad3b4e49144af57353f85e654a30c5
CarnoZhao <273806108@qq.com>
2019-04-29 10:12:27 -0400

    barcodeCount

--- SequencingHomework/temp.py
+++ SequencingHomework/temp.py
@@ -7,7 +7,7 @@ Todo:
 
     3. How many total reads
 
-    4. The distribution of number of reads per barcode
+    4. The distribution of read length per barcode
 
     5. The total number of bases sequenced per barcodes
 
@@ -18,5 +18,38 @@ Todo:
     *8. Assembly the genome and the assembly statistics
 '''
 
+from Bio import SeqIO, Seq
+import gzip
+from collections import defaultdict
+
 class Genome():
-    def __init__(self, )
+    def __init__(self, filename):
+        self.filename = filename
+
+    def load(self):
+        '''
+        Load the file to a generator to save memory
+        '''
+        try:
+            self.f = SeqIO.parse(self.filename, 'fastq')
+        except:
+            self.f = SeqIO.parse(gzip.open(self.filename, 'rt', encoding = 'utf-8'), 'fastq')
+
+    def barcode_statistics(self):
+        '''
+        Count the number of barcodes and the number of reads per barcode.
+        And do further analysis.
+        (Todo 1-5: DONE !)
+        '''
+        self.load()
+        barcodes = defaultdict(int)
+        for rec in self.f:
+            des = rec.description
+            barcode = des.split(' ')[-1].split('=')[-1]
+            barcodes[barcode] += 1
+        print('In file %s, there are %d barcodes.' % (self.filename.split('/')[-1], len(barcodes)))
+        for barcode, num in barcodes.items():
+            print('For %s, there are %d reads' % (barcode, num))
+
+    def 
+        
