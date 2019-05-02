263a2356ae2f44a930076419f3076499636ca760
CarnoZhao <273806108@qq.com>
2019-05-01 22:17:00 -0400

    phylo2

--- SequencingHomework/analysis/entrez.py
+++ SequencingHomework/analysis/entrez.py
@@ -1,5 +1,6 @@
 from Bio import Entrez, SeqIO, Seq, GenBank
 import pandas as pd
+import collections
 
 Entrez.email = 'xz2827@columbia.edu'
 
@@ -7,23 +8,26 @@ def func():
     data = pd.read_csv('../../../DataLists/barcode01.out', sep = '\t')
 
     data.columns = ['qaccver', 'saccver', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']
-    ret = data['saccver'][:10].tolist()
+    ret = data['saccver'].tolist()
     print(ret)
     return ret
 
 def get_org(accs):
-    phylo = 'kingdom-phylum-class-order-family-genus-species'.split('-')
-    df = dict((x, []) for x in phylo)
+    # phylo = 'kingdom-phylum-class-order-family-genus-species'.split('-')
+    #df = dict((x, []) for x in phylo)
+    orgs = []
     hdl = Entrez.efetch(db = 'nucleotide', id = accs, rettype = 'gb')
     recs = GenBank.parse(hdl)
     for i, rec in enumerate(recs):
-        tax = rec.taxonomy
+        # tax = rec.taxonomy
         org = rec.organism
-        print(tax)
-        for j, t in enumerate(tax):
-            df[phylo[j]].append(t)
-        df[phylo[j + 1]].append(org)
-    print(pd.DataFrame(df))
+        orgs.append(org)
+        # print(tax)
+        # for j, t in enumerate(tax):
+        #   df[phylo[j]].append(t)
+        # df[phylo[j + 1]].append(org)
+    # print(pd.DataFrame(df))
+    print(collections.Counter(orgs))
 
 get_org(func())
 
