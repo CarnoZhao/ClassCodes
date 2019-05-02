d9ee73e258d75e9caffb803b26c54ee73692a7ec
CarnoZhao <273806108@qq.com>
2019-05-02 10:57:26 -0400

    buildOrgs

--- SequencingHomework/analysis/entrez.py
+++ SequencingHomework/analysis/entrez.py
@@ -6,28 +6,33 @@ Entrez.email = 'xz2827@columbia.edu'
 
 def func():
     data = pd.read_csv('../../../DataLists/barcode01.out', sep = '\t')
-
     data.columns = ['qaccver', 'saccver', 'pident', 'length', 'mismatch', 'gapopen', 'qstart', 'qend', 'sstart', 'send', 'evalue', 'bitscore']
-    ret = data['saccver'].tolist()
-    print(ret)
-    return ret
+    data = data.groupby(data.qaccver).apply(lambda x: x[['saccver', 'bitscore']][x.bitscore == max(x.bitscore)])
+    # ret = data['saccver'].tolist()
+    return data
 
 def get_org(accs):
     # phylo = 'kingdom-phylum-class-order-family-genus-species'.split('-')
-    #df = dict((x, []) for x in phylo)
+    # df = dict((x, []) for x in phylo)
     orgs = []
-    hdl = Entrez.efetch(db = 'nucleotide', id = accs, rettype = 'gb')
+    fw = open('orgs.txt', 'w')
+    hdl = Entrez.efetch(db = 'nucleotide', id = accs.saccver.tolist(), rettype = 'gb')
     recs = GenBank.parse(hdl)
     for i, rec in enumerate(recs):
         # tax = rec.taxonomy
         org = rec.organism
-        orgs.append(org)
+        fw.write(org + '\n')
         # print(tax)
+        print(org)
+        orgs.append(org)
         # for j, t in enumerate(tax):
         #   df[phylo[j]].append(t)
         # df[phylo[j + 1]].append(org)
     # print(pd.DataFrame(df))
     print(collections.Counter(orgs))
+    fw.close()
 
-get_org(func())
+res = func()
+# print(res.head(10))
+get_org(res)
 
